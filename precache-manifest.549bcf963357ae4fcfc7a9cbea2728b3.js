c1ed86252c727d00d768d95cce3cdd25b20360cb
Tyranno <eotjdgkdl@gmail.com>
2019-10-12 22:56:06 +0900

    Updates

---
+++ precache-manifest.549bcf963357ae4fcfc7a9cbea2728b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a54d8095b94f3bcc8417058b8bd452f",
+    "url": "/DS_Blog/index.html"
+  },
+  {
+    "revision": "80b62c4a624fa39da837",
+    "url": "/DS_Blog/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2c96237edad97c300536",
+    "url": "/DS_Blog/static/js/2.f5db97f5.chunk.js"
+  },
+  {
+    "revision": "80b62c4a624fa39da837",
+    "url": "/DS_Blog/static/js/main.1bd96ae6.chunk.js"
+  },
+  {
+    "revision": "42b606e41f077fde18d9",
+    "url": "/DS_Blog/static/js/runtime-main.aa1e2b18.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/DS_Blog/static/media/logo.25bf045c.svg"
+  }
+]);
